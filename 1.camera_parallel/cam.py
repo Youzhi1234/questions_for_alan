55492930d8ed07f538a92f34e43becd55b23cac5
Youzhi1234 <89746673+Youzhi1234@users.noreply.github.com>
2023-06-10 20:01:50 -0400

    Update cam.py

--- 1.camera_parallel/cam.py
+++ 1.camera_parallel/cam.py
@@ -6,6 +6,7 @@ import mvsdk
 from datetime import datetime
 import os
 import time
+from multiprocessing import Queue,Process
 
 class cam:
     def __init__(self,idx):
@@ -28,19 +29,19 @@ class cam:
         cap = mvsdk.CameraGetCapability(self.hCamera)
         monoCamera = (cap.sIspCapacity.bMonoSensor != 0)
 
-        RoIImage = mvsdk.tSdkImageResolution()
-        RoIImage.iIndex = 255
-        RoIImage.iWidthFOV = 648 
-        RoIImage.iHeightFOV = 486 
-        RoIImage.iWidth = 648 
-        RoIImage.iHeight = 486
-        mvsdk.CameraSetImageResolution(self.hCamera,RoIImage)      
+        # RoI Binning
+        # RoIImage = mvsdk.tSdkImageResolution()
+        # RoIImage.iIndex = 255
+        # RoIImage.iWidthFOV = 648 
+        # RoIImage.iHeightFOV = 486 
+        # RoIImage.iWidth = 648 
+        # RoIImage.iHeight = 486
+        # mvsdk.CameraSetImageResolution(self.hCamera,RoIImage)      
 
         mvsdk.CameraSetIspOutFormat(self.hCamera, mvsdk.CAMERA_MEDIA_TYPE_BGR8)
         mvsdk.CameraSetTriggerMode(self.hCamera, 0)
         mvsdk.CameraSetAeState(self.hCamera, 1)
-        mvsdk.CameraSetAeExposureRange(self.hCamera,1*1000,10*1000)
-        
+        mvsdk.CameraSetAeExposureRange(self.hCamera,100*1000,100*1000)
         mvsdk.CameraPlay(self.hCamera)
         
         FrameBufferSize = cap.sResolutionRange.iWidthMax * cap.sResolutionRange.iHeightMax * (1 if monoCamera else 3)
@@ -52,97 +53,118 @@ class cam:
         frame_data = (mvsdk.c_ubyte * FrameHead.uBytes).from_address(self.pFrameBuffer)
         frame = np.frombuffer(frame_data, dtype=np.uint8)
         frame = frame.reshape((FrameHead.iHeight, FrameHead.iWidth, 1 if FrameHead.uiMediaType == mvsdk.CAMERA_MEDIA_TYPE_MONO8 else 3))
-        print(frame.shape)
+        # print(frame.shape)
         frame = cv2.resize(frame, (512,512))
         return frame
     def kill(self):
         mvsdk.CameraUnInit(self.hCamera)
         mvsdk.CameraAlignFree(self.pFrameBuffer)
 
-
-if __name__ == "__main__":
-    # now = datetime.now()
-    # dt_string = now.strftime("%Y-%m-%d_%H-%M-%S")
-    # print(dt_string)
-    # cam_1_folder=f"datasets/TTCI/video/{dt_string}/01"
-    # cam_2_folder=f"datasets/TTCI/video/{dt_string}/02"
-    # cam_3_folder=f"datasets/TTCI/video/{dt_string}/03"
-    # cam_4_folder=f"datasets/TTCI/video/{dt_string}/04"
-    # os.makedirs(cam_1_folder)
-    # os.makedirs(cam_2_folder)
-    # os.makedirs(cam_3_folder)
-    # os.makedirs(cam_4_folder)
-
-    cam_1=cam(0)
-    cam_2=cam(1)
-    # cam_3=cam(2)
-    # cam_4=cam(3)
-    idx=0
-    while True:
-        t=time.time()
-        frames=[]
-        frame1 = cam_1.read()
-        frames.append(frame1)
-        frame2 = cam_2.read()
-        # frames.append(frame2)
-        # frame3 = cam_3.read()
-        # frames.append(frame3)
-        # frame4 = cam_4.read()
-        # frames.append(frame4)
-        # cv2.imwrite(cam_1_folder+'/'+str(idx).zfill(5)+'.jpg',frame1)
-        # cv2.imwrite(cam_2_folder+'/'+str(idx).zfill(5)+'.jpg',frame2)
-        # cv2.imwrite(cam_3_folder+'/'+str(idx).zfill(5)+'.jpg',frame3)
-        # cv2.imwrite(cam_4_folder+'/'+str(idx).zfill(5)+'.jpg',frame4)
-        
-        frames_1=np.hstack(frames[:2])
-        # frames_2=np.hstack(frames[2:])
-        # frames=np.vstack([frames_1,frames_2])
-
-        cv2.imshow('0',frames_1)
-        cv2.waitKey(1)
-        idx+=1
-        # time.sleep(0.1)
-        print(time.time()-t)
-        
-
-        
-
-# def read_frames(cam_idx, frame_queue, trigger_queue):
-#     cam_instance = cam(cam_idx)
-#     while True:
-#         # Wait for the trigger signal
-#         trigger = trigger_queue.get()
-#         if trigger:
-#             frame = cam_instance.read()
-#             frame_queue.put(frame)
+'''
+single-processor
+'''
 
 # if __name__ == "__main__":
-#     frame_queue_1 = Queue(0)
-#     frame_queue_2 = Queue(0)
-#     trigger_queue_1 = Queue(0)
-#     trigger_queue_2 = Queue(0)
-
-#     process_1 = Process(target=read_frames, args=(0, frame_queue_1, trigger_queue_1))
-#     process_2 = Process(target=read_frames, args=(1, frame_queue_2, trigger_queue_2))
-
-#     process_1.start()
-#     process_2.start()
-
+#     cam_1=cam(0)
+#     cam_2=cam(1)
+#     cam_3=cam(2)
+#     cam_4=cam(3)
+#     idx=0
 #     while True:
-#         # Send trigger signals to both processes
-#         T = time.time()
-#         trigger_queue_1.put(True)
-#         trigger_queue_2.put(True)
-#         frame1 = frame_queue_1.get()
-#         print(time.time() - T)
-#         frame2 = frame_queue_2.get()
-#         print(time.time() - T)
-#         cv2.imshow('0',frame1)
-#         cv2.imshow('1',frame2)
-#         cv2.waitKey(1)
-        
-#         # print(frame1.shape)
-#         # print(frame2.shape)
+#         print("=============={} frames=============".format(idx))
+#         t0=time.time()
+#         frame1 = cam_1.read()
+#         t1=time.time()
+#         print("cam_1 spends {} s".format(t1-t0))
+#         frame2 = cam_2.read()
+#         t2=time.time()
+#         print("cam_2 spends {} s".format(t2-t1))
+#         frame3 = cam_3.read()
+#         t3=time.time()
+#         print("cam_3 spends {} s".format(t3-t2))
+#         frame4 = cam_4.read()
+#         t4=time.time()
+#         print("cam_4 spends {} s".format(t4-t3))
+#         print("total time {} s".format(t4-t0))
+#         idx+=1
+#         # frames=[]
+#         # frames.append(frame1)
+#         # frames.append(frame2)
+#         # frames.append(frame3)
+#         # frames.append(frame4)
+#         # frames_1=np.hstack(frames[:2])
+#         # frames_2=np.hstack(frames[2:])
+#         # frames=np.vstack([frames_1,frames_2])
 
-#     process_1.join()
-#     process_2.join()
+#         # cv2.imshow('0',frame1)
+#         # cv2.waitKey(1)
+        
+    
+
+def read_frames(cam_idx, frame_queue, trigger_queue):
+    cam_instance = cam(cam_idx)
+    while True:
+        # Wait for the trigger signal
+        trigger = trigger_queue.get()
+        if trigger:
+            frame = cam_instance.read()
+            frame_queue.put(frame)
+'''
+mutil-processor
+'''
+if __name__ == "__main__":
+    frame_queue_1 = Queue(0)
+    frame_queue_2 = Queue(0)
+    frame_queue_3 = Queue(0)
+    frame_queue_4 = Queue(0)
+
+    trigger_queue_1 = Queue(0)
+    trigger_queue_2 = Queue(0)
+    trigger_queue_3 = Queue(0)
+    trigger_queue_4 = Queue(0)
+
+    process_1 = Process(target=read_frames, args=(0, frame_queue_1, trigger_queue_1))
+    process_2 = Process(target=read_frames, args=(1, frame_queue_2, trigger_queue_2))
+    process_3 = Process(target=read_frames, args=(2, frame_queue_3, trigger_queue_3))
+    process_4 = Process(target=read_frames, args=(3, frame_queue_4, trigger_queue_4))
+
+    process_1.start()
+    process_2.start()
+    process_3.start()
+    process_4.start()
+    idx=0
+    while True:
+        # Send trigger signals to both processes
+        print("=============={} frames=============".format(idx))
+        trigger_queue_1.put(True)
+        trigger_queue_2.put(True)
+        trigger_queue_3.put(True)
+        trigger_queue_4.put(True)
+        t0=time.time()
+        frame1 = frame_queue_1.get()
+        t1=time.time()
+        print("cam_1 spends {} s".format(t1-t0))
+        frame2 = frame_queue_2.get()
+        t2=time.time()
+        print("cam_2 spends {} s".format(t2-t1))
+        frame3 = frame_queue_3.get()
+        t3=time.time()
+        print("cam_3 spends {} s".format(t3-t2))
+        frame4 = frame_queue_4.get()
+        t4=time.time()
+        print("cam_4 spends {} s".format(t4-t3))
+        print("total time {} s".format(t4-t0))
+        idx+=1
+
+# #         # frames=[]
+# #         # frames.append(frame1)
+# #         # frames.append(frame2)
+# #         # frames.append(frame3)
+# #         # frames.append(frame4)
+# #         # frames_1=np.hstack(frames[:2])
+# #         # frames_2=np.hstack(frames[2:])
+# #         # frames=np.vstack([frames_1,frames_2])
+
+# #         # cv2.imshow('0',frames)
+# #         # cv2.waitKey(1)
+        
